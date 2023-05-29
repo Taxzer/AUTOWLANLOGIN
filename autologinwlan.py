af3ed46887543f3c10960fdd8538dc9a6468f1a7
TwoOnefour <lys214412@gmail.com>
2023-05-30 07:52:28 +0800

    Update autologinwlan.py

--- autologinwlan.py
+++ autologinwlan.py
@@ -23,6 +23,24 @@ class AutoLoginWLAN:
             return False
         else:
             return True
+    
+    def get_connection_status(self):
+        try:
+            res = requests.get("http://172.30.21.100/api/account/status", params={
+                "token": ""
+            })
+            res_text = res.json()
+            status_code = res_text.get("code")
+            if status_code == 1:
+                print("设备不在线，尝试连接")
+                return False
+            elif status_code == 0:
+                print("已经联网，退出")
+                return True  
+        except Exception as e:
+            print("未连接到学校wifi， 退出")
+            return True  #  由于没有链接到学校wifi，不可能访问172.30.21.100，所以必将连接不上，直接返回成功连接
+        
     def login(self):
         headers = {
             "Host":"172.30.21.100",
@@ -35,28 +53,41 @@ class AutoLoginWLAN:
             "username":"", # 填入自己的上网账号
             "password":"", # 填入自己的上网密码
             "nasId":14,
-            "userIpv4": self.result["ip"],  # 可以改成对应ip和mac，指定上网
-            "userMac": self.result["mac"], # 50-C2-E8-77-41-D7 self.result["mac"]
+            "userIpv4": "",  # 可以改成对应ip和mac，指定上网 self.result["ip"]
+            "userMac": "", # 50-C2-E8-77-41-D7 self.result["mac"]
             "captcha":"", # 不想做验证码，因为自己也没遇到过几次
             "captchaId":"",
-            "switchip":self.result["switchip"] # 更新以后必须填switch ip，否则登陆不了
+            "switchip":"" # 更新以后必须填switch ip，否则登陆不了 self.result["switchip"] 好像可以乱填
         }
         res = requests.post("http://172.30.21.100/api/account/login",data=data)
         data = json.loads(res.text)
         if data["code"] != 0:
             print("登陆失败")
+            return False
+        else:
+            return True
+        
+    def logout(self):  # 写一个登出接口，方便调试
+        requests.get("http://172.30.21.100/api/account/logout", params={
+            "token": ""
+        })
+
 if __name__ == "__main__":
     disable_warnings()
     loginProcess = AutoLoginWLAN()
     errorCount = 0
     # time.sleep(60) # 等待网卡就绪
     while True:
-        if not loginProcess.testConnection():
-            loginProcess.login()
+        if not loginProcess.get_connection_status():
+            # loginProcess.login()
+            if loginProcess.login():
+                print("成功联网")
+                break
             time.sleep(5) # 五秒重复一次登录
             errorCount += 1
         else:
-            print("已经联网，退出")
             break
         if errorCount > 50: # 50次失败自动退出
             break
+    # loginProcess.login()
+    # loginProcess.logout()
